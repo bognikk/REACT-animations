bc59c1d7b7629cc28a7c0da1863555aee862d760
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-04-12 16:27:11 -0400

    starting setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import "./App.scss";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>React Animations</h1>
+			<Modal />
+			<Backdrop />
+			<button className="Button">Open Modal</button>
+			<h3>Animating Lists</h3>
+			<List />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Backdrop/Backdrop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Backdrop.scss";
+
+const backdrop = (props) => <div className="Backdrop"></div>;
+
+export default backdrop;
